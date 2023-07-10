487a20ba15f1ca5a50d3b8293ec16bcd7c4fe5ac
Stan Tarasenko <stan.tarasenko1991@gmail.com>
2023-07-10 23:10:58 +0300

    work with app frame

---
+++ src/mockBase/coursesBase.js
@@ -0,0 +1,44 @@
+const coursesBaseUK = [
+  {
+    id: '1',
+    title: 'Курс великодньої флористики',
+    shortInfo: 'До свята важливо запропонувати нові ідеї для своїх клієнтів. Навчимо вас створювати цікавий тематичний каркасний букет…',
+    image: 'https://www.elitbuketschool.art/wp-content/uploads/elementor/thumbs/IMG_4103-scaled-p5emrxo7dcx0iu3zcvvzw8rru3pwouoqy1ora159jc.jpg',
+    mainImage: 'https://www.elitbuketschool.art/wp-content/uploads/2021/04/flat-lay-box-with-colorful-easter-eggs-copy-space-scaled.jpg',
+    fullInfo: 'В этом уроке вы увидите как можно создать своими руками прекрасную основу для пасхальной композиции. Яйцо из алебастра — оригинально можно использовать в пасхальной флористике как для срезанных цветов — как и для луковичных растений. На видео мы сделаем вариант сложнее — с композицией на оазисе. Так же много комментариев в процессе о расстановке материалов и тонкостях создания.',
+    price: '350',
+    variants: [],
+  },
+  {
+    id: '2',
+    title: 'Курс: Level up. Каркасні букети',
+    shortInfo: 'У курсі «Level up. Каркасні букети» зібрані найпопулярніші та базові техніки каркасних букетів. Кожен із 5 уроків практичний — і несе у собі величезну кількість варіацій. Цих технік ще не було у наших відеоуроках. Це абсолютно новий унікальний курс.',
+    image: 'https://www.elitbuketschool.art/wp-content/uploads/elementor/thumbs/IMG_4699-pv1imlitez22y864ttmk1086f2t8hso4es9hgx1mdk.jpg',
+    mainImage: 'https://www.elitbuketschool.art/wp-content/uploads/2021/04/flat-lay-box-with-colorful-easter-eggs-copy-space-scaled.jpg',
+    fullInfo: 'У курсі «Level up. Каркасні букети» зібрані найпопулярніші та базові техніки каркасних букетів. Кожен із 5 уроків практичний — і несе у собі величезну кількість варіацій. Цих технік ще не було у наших відеоуроках. Це абсолютно новий унікальний курс. Тонкощі складання кожного виду каркасу. Секрети — щоб робота виглядала дорого і продається !',
+    price: '1000',
+    variants: [],
+  },
+  {
+    id: '3',
+    title: 'Майстер-клас "Правило гарної фотографії для флористів"',
+    shortInfo: 'Відеоурок та путівник, який допоможе вам створювати красиві професійні фото ваших робіт. Для кожного флориста важливо сказати про себе.',
+    image: 'https://www.elitbuketschool.art/wp-content/uploads/elementor/thumbs/IMG_0336-scaled-przmnu1dqdzdyrk60raajdj1k91xx8xij5gqqeu1e0.jpg',
+    mainImage: 'https://www.elitbuketschool.art/wp-content/uploads/2021/04/flat-lay-box-with-colorful-easter-eggs-copy-space-scaled.jpg',
+    fullInfo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    price: '500',
+    variants: [],
+  },
+  {
+    id: '4',
+    title: 'Базовий курс',
+    shortInfo: 'Усі основні знання, з якими ви зможете зайти у флористику професіоналом!',
+    image: 'https://www.elitbuketschool.art/wp-content/uploads/elementor/thumbs/IMG_0476-scaled-p54o42tx2vk3zzawj5xgg07eoqm8reieqafjezonm0.jpg',
+    mainImage: 'https://www.elitbuketschool.art/wp-content/uploads/2021/04/flat-lay-box-with-colorful-easter-eggs-copy-space-scaled.jpg',
+    fullInfo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    price: '450',
+    variants: [],
+  },
+];
+
+export default coursesBaseUK;
